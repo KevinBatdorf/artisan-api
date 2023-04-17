c99ef1ca8dfbe0125558c1945e0bd8e28ae858b9
Kevin Batdorf <kbat82@gmail.com>
2023-04-17 17:13:32 -0400

    Add v10 to endpoint

--- pages/api/_data/index.ts
+++ pages/api/_data/index.ts
@@ -2,11 +2,13 @@ import v6 from './6.x.json'
 import v7 from './7.x.json'
 import v8 from './8.x.json'
 import v9 from './9.x.json'
+import v10 from './10.x.json'
 
 const commands = {
     '6.x': v6,
     '7.x': v7,
     '8.x': v8,
     '9.x': v9,
+    '10.x': v10,
 }
 export default commands
