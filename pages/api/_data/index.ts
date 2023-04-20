af3b1745c4e8225044e5d4db6acbbb969947d89a
GitHub <github-actions[bot]@users.noreply.github.com>
2023-04-20 13:20:25 +0000

    Build Laravel version v10

--- pages/api/_data/index.ts
+++ pages/api/_data/index.ts
@@ -1,7 +1,7 @@
-import v10 from "10.x.json"
-import v9 from "9.x.json"
-import v8 from "8.x.json"
-import v7 from "7.x.json"
+import v10 from "./10.x.json"
+import v9 from "./9.x.json"
+import v8 from "./8.x.json"
+import v7 from "./7.x.json"
 
 const commands = {
 }
