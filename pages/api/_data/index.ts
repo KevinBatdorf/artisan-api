488befc33ead357040218e98e437105876c7e948
GitHub <github-actions[bot]@users.noreply.github.com>
2025-12-01 14:01:57 +0000

    Build Laravel version v11

--- pages/api/_data/index.ts
+++ pages/api/_data/index.ts
@@ -1,18 +1,10 @@
 import v12 from "./12.x.json"
 import v11 from "./11.x.json"
 import v10 from "./10.x.json"
-import v9 from "./9.x.json"
-import v8 from "./8.x.json"
-import v7 from "./7.x.json"
-import v6 from "./6.x.json"
 
 const commands = {
 	"12.x": v12,
 	"11.x": v11,
 	"10.x": v10,
-	"9.x": v9,
-	"8.x": v8,
-	"7.x": v7,
-	"6.x": v6,
 }
 export default commands
